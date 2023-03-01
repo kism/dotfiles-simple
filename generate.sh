1de84632e84e44de6e636a98ec86908500ce3965
Kieran Gee <kieran.lost.the.game@gmail.com>
2023-03-01 13:11:59 +0800

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,25 @@
+echo > generated.txt
+
+echo 'cat > ~/.bashrc << "EOF"' >> generated.txt
+cat .bashrc >> generated.txt
+echo "EOF" >> generated.txt
+
+echo >> generated.txt
+
+echo 'cat > ~/.bash_profile << "EOF"' >> generated.txt
+cat .bash_profile >> generated.txt
+echo "EOF" >> generated.txt
+
+echo >> generated.txt
+
+echo 'cat > ~/.inputrc << "EOF"' >> generated.txt
+cat .inputrc >> generated.txt
+echo "EOF" >> generated.txt
+
+echo >> generated.txt
+
+echo 'cat > ~/.vimrc << "EOF"' >> generated.txt
+cat .vimrc >> generated.txt
+echo "EOF" >> generated.txt
+
+echo >> generated.txt
