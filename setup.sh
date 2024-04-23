db7da2a6d0fcc30f35145e692e975739e7ddeb0f
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-04-23 12:51:43 +0800

    restructure

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
-# Copy all the dotfiles
+# Download all the dotfiles
+# Bash
 curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.bash_profile > ~/.bash_profile
 curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.bashrc > ~/.bashrc
 curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.inputrc > ~/.inputrc
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.tmux.conf > ~/.tmux.conf
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.vimrc > ~/.vimrc
-mkdir -p ~/.config/htop/
-curl --silent https://raw.githubusercontent.com/kism/dotfiles-simple/master/htoprc > ~/.config/htop/htoprc
 
-# If this is setting up the root user account, make the tmux bar red
-if [ $EUID -eq 0 ] ; then
+# Tmux
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.tmux.conf > ~/.tmux.conf
+if [ $EUID -eq 0 ] ; then # If this is setting up the root user account, make the tmux bar red
     sed -i 's/green/red/g' ~/.tmux.conf
     sed -i 's/colour10/colour9/g' ~/.tmux.conf
 fi
+
+# Vim
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles-simple/master/.vimrc > ~/.vimrc
+
+# Htop
+mkdir -p ~/.config/htop/
+curl --silent https://raw.githubusercontent.com/kism/dotfiles-simple/master/htoprc/.config/htop/htoprc > ~/.config/htop/htoprc
 
 echo "Dotfiles setup, exit this shell and start a new one"
